d427e79fc3fd76d1d744d088afce13c8d92e143e
revol-xut <revol-xut@protonmail.com>
2022-10-16 10:59:08 +0200

    quickly fixed nix build

--- derivation.nix
+++ derivation.nix
@@ -1,18 +1,5 @@
-let
-  nodeDependencies = (pkgs.callPackage ./default.nix {}).nodeDependencies;
-in
-
-stdenv.mkDerivation {
+{pkgs, lib, config, stdenv, domain, mkYarnPackage}:
+mkYarnPackage {
   name = "windshield";
   src = ./.;
-  buildInputs = [nodejs];
-  buildPhase = ''
-    ln -s ${nodeDependencies}/lib/node_modules ./node_modules
-    export PATH="${nodeDependencies}/bin:$PATH"
-
-    # Build the distribution bundle in "dist"
-    ng build
-
-    cp -r dist $out/
-  '';
 }
