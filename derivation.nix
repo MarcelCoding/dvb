b08ff36783ef239eca1e5637c7b2d5f80f2cad16
revol-xut <revol-xut@protonmail.com>
2022-10-16 12:00:33 +0200

    decompressed yarn build artifact

--- derivation.nix
+++ derivation.nix
@@ -1,5 +1,25 @@
-{pkgs, lib, config, stdenv, domain, mkYarnPackage}:
-mkYarnPackage {
+{ pkgs, lib, config, stdenv, domain, mkYarnPackage }:
+stdenv.mkDerivation {
   name = "windshield";
-  src = ./.;
+  src = mkYarnPackage {
+    name = "windshield-compressed";
+    src = ./.;
+  };
+
+  buildPhase = ''
+    mkdir -p ./decompressed
+
+    ls -alh
+    ls -alh tarballs
+  
+    cd decompressed 
+    tar xf ../tarballs/windshield-compressed.tgz
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    
+    ls -alh
+    cp -r ./package/src/* $out/bin
+  '';
 }
